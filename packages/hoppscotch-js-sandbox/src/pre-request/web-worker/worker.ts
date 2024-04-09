dee629a5d2b3c6f1ef71cd01850a1d9a78c6e34c
ChingCdesu <chingc.ccmail@gmail.com>
2024-04-09 20:41:46 +0800

    feat: pre-script support top-level async/await

--- packages/hoppscotch-js-sandbox/src/pre-request/web-worker/worker.ts
+++ packages/hoppscotch-js-sandbox/src/pre-request/web-worker/worker.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import * as TE from "fp-ts/TaskEither"
 
 import { TestResult } from "~/types"
@@ -7,19 +8,25 @@ const executeScriptInContext = (
   preRequestScript: string,
   envs: TestResult["envs"]
 ): TE.TaskEither<string, TestResult["envs"]> => {
-  try {
-    const { pw, updatedEnvs } = getPreRequestScriptMethods(envs)
+  const { pw, updatedEnvs } = getPreRequestScriptMethods(envs)
 
-    // Create a function from the pre request script using the `Function` constructor
-    const executeScript = new Function("pw", preRequestScript)
+  const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor
 
-    // Execute the script
-    executeScript(pw)
+  // Create a function from the pre request script using the `Function` constructor
+  const asyncExecuteScript = new AsyncFunction("pw", preRequestScript)
 
-    return TE.right(updatedEnvs)
-  } catch (error) {
-    return TE.left(`Script execution failed: ${(error as Error).message}`)
-  }
+  const execute = TE.tryCatchK(
+    async () => {
+      await asyncExecuteScript(pw)
+      return updatedEnvs
+    },
+    (error) => `Script execution failed: ${(error as Error).message}`
+  )
+
+  // Execute the script
+  const result = execute()
+
+  return result
 }
 
 // Listen for messages from the main thread
